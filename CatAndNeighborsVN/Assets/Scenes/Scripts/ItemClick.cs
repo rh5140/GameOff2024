d995dec347cb4eaaf7f539a82d140238f79fabac
Ray Hsiao <40257341+rh5140@users.noreply.github.com>
2024-11-16 01:21:11 -0800

    Deleted Popup.cs, added color change for cursor hover

--- CatAndNeighborsVN/Assets/Scenes/Scripts/ItemClick.cs
+++ CatAndNeighborsVN/Assets/Scenes/Scripts/ItemClick.cs
@@ -1,7 +1,8 @@
 using UnityEngine.EventSystems;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class ItemClick : MonoBehaviour, IPointerClickHandler
+public class ItemClick : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
 { 
     public GameObject popup;
     public GameObject closeButton;
@@ -10,5 +11,18 @@ public class ItemClick : MonoBehaviour, IPointerClickHandler
     {
         popup.SetActive(true);
         closeButton.SetActive(true);
+    }
+
+    public void OnPointerEnter(PointerEventData pointerEventData)
+    {
+        // Changes image color to red on hover
+        GetComponent<Image>().color = new Color(255, 0, 0 );
+    }
+
+    //Detect when Cursor leaves the GameObject
+    public void OnPointerExit(PointerEventData pointerEventData)
+    {
+        // Changes image color to white
+        GetComponent<Image>().color = new Color(255, 255, 255);
     }
 }
