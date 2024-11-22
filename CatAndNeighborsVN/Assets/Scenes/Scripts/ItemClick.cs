fc62f2bbc3d89681a0734c63cbef3ce7e12f443f
Ray Hsiao <40257341+rh5140@users.noreply.github.com>
2024-11-21 23:37:35 -0800

    Change cursor on hover and start dialogue nodes on click
    - Using very temporary assets (starry bg for cursor, Character_Select for node)
    - Need to clean up ItemClick.cs and scene later (no longer need text popups)

--- CatAndNeighborsVN/Assets/Scenes/Scripts/ItemClick.cs
+++ CatAndNeighborsVN/Assets/Scenes/Scripts/ItemClick.cs
@@ -1,33 +1,42 @@
 using UnityEngine.EventSystems;
 using UnityEngine;
 using UnityEngine.UI;
+using Yarn.Unity;
 
 public class ItemClick : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
 { 
     [SerializeField] private GameObject popup;
     [SerializeField] private GameObject inspectImage;
     [SerializeField] private bool changesItemState;
+    [SerializeField] private string node;
+    [SerializeField] private Texture2D cursorTexture;
+    private DialogueRunner dialogueRunner;
+
+    public void Start() {
+        dialogueRunner = FindObjectOfType<Yarn.Unity.DialogueRunner>();
+    }
 
     public void OnPointerClick(PointerEventData eventData)
     {
+        if (dialogueRunner.IsDialogueRunning) return;
         // text popup
         popup.SetActive(true);
+        dialogueRunner.StartDialogue(node);
 
         // replace current image with new image
         if (changesItemState) inspectImage.SetActive(true);
+        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
         gameObject.SetActive(false);
     }
 
     public void OnPointerEnter(PointerEventData pointerEventData)
     {
-        // Changes image color to red on hover
-        GetComponent<Image>().color = new Color(255, 0, 0 );
+        Cursor.SetCursor(cursorTexture, Vector2.zero, CursorMode.Auto);
     }
 
     //Detect when Cursor leaves the GameObject
     public void OnPointerExit(PointerEventData pointerEventData)
     {
-        // Changes image color to white
-        GetComponent<Image>().color = new Color(255, 255, 255);
+        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
     }
 }
