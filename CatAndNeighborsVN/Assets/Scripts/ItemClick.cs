c6fd701c77495917ec8be8854987c249304931d2
Ray Hsiao <40257341+rh5140@users.noreply.github.com>
2024-11-28 19:06:53 -0800

    Revert "Merge pull request #12 from rh5140/doorOpen"
    This reverts commit ab514675d59c781f754c8d97ef97c0132dcca393, reversing
    changes made to 360d792ffb455bd73067c8e20006ff5bbdbc5793.

--- CatAndNeighborsVN/Assets/Scripts/ItemClick.cs
+++ CatAndNeighborsVN/Assets/Scripts/ItemClick.cs
@@ -28,7 +28,7 @@ public class ItemClick : MonoBehaviour, IPointerClickHandler, IPointerEnterHandl
 
     public void OnPointerEnter(PointerEventData pointerEventData)
     {
-        Cursor.SetCursor(cursorTexture, Vector2.zero, CursorMode.Auto);
+        Cursor.SetCursor(cursorTexture, Vector2.zero, CursorMode.ForceSoftware);
     }
 
     //Detect when Cursor leaves the GameObject
