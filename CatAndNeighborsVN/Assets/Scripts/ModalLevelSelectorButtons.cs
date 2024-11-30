6fe8f43a2ddb92be57e5ff4dcc3182d3bc6f627b
jane-le-huynh <47515857+jane-le-huynh@users.noreply.github.com>
2024-11-30 14:58:11 -0800

    made levelselectormodal a prefab for all levels

--- CatAndNeighborsVN/Assets/Scripts/ModalLevelSelectorButtons.cs
+++ CatAndNeighborsVN/Assets/Scripts/ModalLevelSelectorButtons.cs
@@ -1,4 +1,6 @@
+using UnityEngine.EventSystems;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class LevelSelectorModal : MonoBehaviour
@@ -7,6 +9,4 @@ public class LevelSelectorModal : MonoBehaviour
     public void ChangeScene(string sceneName) {
         SceneManager.LoadScene(sceneName);
     }
-
-    // transition effect to load in?
 }
